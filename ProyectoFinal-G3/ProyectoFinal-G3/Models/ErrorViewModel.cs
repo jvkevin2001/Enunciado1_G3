6a3b973d292de09663db582346368cff0b5abfb0
JR725 <123512232+JR725@users.noreply.github.com>
2025-06-04 12:51:14 -0600

    Jorge - Commit 1
    Proyecto .Net Core
    Login
    Plantilla Login sin Errores

---
+++ ProyectoFinal-G3/ProyectoFinal-G3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyectoFinal_G3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
