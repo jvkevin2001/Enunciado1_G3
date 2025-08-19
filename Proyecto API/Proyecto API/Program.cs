826375fcfc8aca3b6d6da2cf047ec728daeb750d
SebesHJDEV <Sebastianhernandezjimenez019@gmail.com>
2025-08-18 20:06:53 -0600

    login

--- Proyecto API/Proyecto API/Program.cs
+++ Proyecto API/Proyecto API/Program.cs
@@ -1,29 +1,82 @@
 using Proyecto_API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
-
-// Add services to the container.
-
+// Add services to the container
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
+// Registrar tus servicios
 builder.Services.AddScoped<IUtil, Util>();
-builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IUtilitarios, Utilitarios>();
+
+// Configurar Swagger
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Proyecto_API",
+        Description = "API de ejemplo con JWT",
+    });
+});
+
+// Configurar JWT
+var llaveSegura = builder.Configuration["Start:LlaveSegura"]!;
+builder.Services.AddAuthentication(opt =>
+{
+    opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(opt =>
+{
+    opt.RequireHttpsMetadata = false;
+    opt.SaveToken = true;
+    opt.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(llaveSegura)),
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ClockSkew = TimeSpan.Zero
+    };
+    opt.Events = new JwtBearerEvents
+    {
+        OnChallenge = context =>
+        {
+            context.HandleResponse();
+            context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+            context.Response.ContentType = "application/json";
+
+            var _utilitarios = context.HttpContext.RequestServices.GetRequiredService<IUtilitarios>();
+            var respuesta = _utilitarios.RespuestaIncorrecta("JWTNoValido");
+
+            var result = JsonSerializer.Serialize(respuesta);
+            return context.Response.WriteAsync(result);
+        }
+    };
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseExceptionHandler("/api/Error/CapturarError"); // este de cada 
+app.UseExceptionHandler("/api/Error/CapturarError");
+
 app.UseHttpsRedirection();
 
+// Autenticación y autorización
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
