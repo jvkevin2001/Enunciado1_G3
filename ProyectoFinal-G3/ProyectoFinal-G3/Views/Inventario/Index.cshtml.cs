244a2ffab706c851824afc75d7e4c6af1ff38eb2
jvkevin2001 <Kevin.pereira@pandatechla.com>
2025-08-21 01:35:47 -0600

    Feat-Corrección Inventario Api Feat-Creación FrontEnd de gESTIÓN DE iNVENTARIO

---
+++ ProyectoFinal-G3/ProyectoFinal-G3/Views/Inventario/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProyectoFinal_G3.Views.Inventario
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
